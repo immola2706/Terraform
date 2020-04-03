3fd1bbd7bf33ec82e0a6791399543e2704b422a4
Arthur <immola.chr.arthur@dxc.com>
2020-04-03 16:19:16 +0530

    changes in the files

--- asg_launch_conf.tf
+++ asg_launch_conf.tf
@@ -1,8 +1,8 @@
-resource aws_launch_configuration "my-first-launch-conf" {
+resource "aws_launch_configuration" "my-first-launch-conf" {
 #name = "webserver-launch"
-image_id = var.ami
-instance_type = var.instance_type
-security_groups=[ aws_security_group.webserver_sg.id ]
+image_id = "var.ami"
+instance_type = "var.instance_type"
+security_groups=[ "aws_security_group.webserver_sg.id" ]
 key_name = "terraform"
 user_data = <<-EOF
   #!/bin/bash
